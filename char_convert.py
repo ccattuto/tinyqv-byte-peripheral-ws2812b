4d0b07eea0ca03a61ed24bd016b455ac9fdaf868
Ciro Cattuto <ciro.cattuto@gmail.com>
2025-07-20 18:31:36 +0200

    ROM simplifcation

--- char_convert.py
+++ char_convert.py
@@ -269,7 +269,7 @@ def hex_to_binary_row_major(hex_values):
 				seq.append(0)
 	return seq
 
-for i in range(32, 127):
+for i in range(32, 128):
     bitseq = hex_to_binary_row_major(char_list[i*5 : (i+1)*5])
     print ("".join(['%0d' % digit for digit in bitseq]))
 
--- src/char_rom.v
+++ src/char_rom.v
@@ -2,18 +2,18 @@ module char_rom #(
     parameter DATA_WIDTH = 35,     // Width of ROM data (35 bits for each character)
     parameter ADDR_WIDTH = 7,      // Address width
     parameter ADDR_MIN = 32,
-    parameter ADDR_MAX = 126
+    parameter ADDR_MAX = 127
 )(
     input wire [ADDR_WIDTH-1:0] address,
     output wire [DATA_WIDTH-1:0] data
 );
 
-reg [DATA_WIDTH-1:0] mem [0:ADDR_MAX-ADDR_MIN];
+reg [DATA_WIDTH-1:0] mem [0:ADDR_MAX-ADDR_MIN+1];
 
 initial begin
     $readmemb("font.bin", mem);  // load char bitmaps from file
 end
 
-assign data = (address >= ADDR_MIN && address <= ADDR_MAX) ? mem[address-ADDR_MIN] : '1;
+assign data = (address >= ADDR_MIN) ? mem[address-ADDR_MIN] : '0;
 
 endmodule
